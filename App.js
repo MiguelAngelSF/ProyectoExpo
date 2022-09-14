d8923ea436c815b03a7040fcacb8ac2308fa8618
Miguel Angel Salas Flores <miguelangel.sf@outlook.com>
2022-09-13 23:14:01 -0500

    Feat: New project

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Miguel Angel Salas Flores</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#a2122f',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
